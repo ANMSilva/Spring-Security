9dd87d47e014e0210714cf275f3e211814dbaa8e
EPIC\nipun_s <nipun_s@epiclanka.net>
2024-12-02 09:58:54 +0530

    init commit

---
+++ src/test/java/com/nipun/Spring/Security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nipun.Spring.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
